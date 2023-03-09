a17ea5fa61a9f35369e110c452c649a52e946b46
Sophie Kohrogi <sophkohrogi@gmail.com>
2023-03-08 18:47:17 -0800

    updated

--- src/global/Routes.js
+++ src/global/Routes.js
@@ -3,14 +3,12 @@ import React from 'react';
 import IndexContainer from '../index/IndexContainer';
 import InfoContainer from '../info/InfoContainer';
 import LandingContainer from '../landing/LandingContainer';
-import UnderConstruction from '../landing/UnderConstruction';
 import WorkContainer from '../work/WorkContainer';
 
 function Routes() {
   return (
     <Router className='row'>
-      <UnderConstruction path='/' />
-      {/* <LandingContainer path='/' /> */}
+      <LandingContainer path='/' />
       <InfoContainer path='/info' />
       <IndexContainer path='/index' />
       <WorkContainer path='/work/:id' />
